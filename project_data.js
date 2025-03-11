6c26c2816715aead4f2f8ff7e6736064eac949eb
deoxys263 <deoxys263@gmail.com>
2025-03-11 22:15:25 +0530

    first push

---
+++ project_data.js
@@ -0,0 +1,48 @@
+var project_data = {
+    'cs':{
+        "project_name":"Chartered Studies",
+        "logo":"project_thumb.png", //image will be 330*220
+        "url":"https://testseries.charteredstudies.in/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'Home page',"image":'homepage1.png'},
+            {"name":'Home page',"image":'homepage2.png'},
+            {"name":'List of all batches with filter',"image":'buytest.png'},
+            {"name":'List of all batches with filter',"image":'buytest2.png'},
+            {"name":'Registration Page',"image":'register1.png'},
+            {"name":'Registration Page',"image":'register2.png'},
+            {"name":'Login Page with password/OTP',"image":'login.png'},
+            {"name":'Student Dashboard',"image":'student_dash.png'},
+            {"name":'Details of exam',"image":'exam_details_page.png'},
+            {"name":'MCQ Exam Page',"image":'mcq_exam.png'},
+
+            {"name":'Admin Dashboard',"image":'admin_dash.png'},
+            {"name":'Admin Dashboard',"image":'admin_dash2.png'},
+            {"name":'Student Sheets Listing',"image":'student_sheets.png'},
+            {"name":'Student Uploaded Sheet Details',"image":'uploaded_sheet_details.png'},
+            {"name":'Checked Sheet Upload Page',"image":'checked_sheet_upload_page.png'},
+            {"name":'Discussion Forum',"image":'discussion_forum.png'},
+           
+        ]
+    },
+    'CCP':{
+        "project_name":"Chartered Studies",
+        "logo":"project_thumb.png", //image will be 330*220
+        "url":"https://testseries.charteredstudies.in/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+        ]
+    },
+
+};
