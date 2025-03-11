3480ffd7e6ae5be97580ed5f0c176d3f2c7669e7
deoxys263 <deoxys263@gmail.com>
2025-03-11 23:08:26 +0530

    some projects logo

--- project_data.js
+++ project_data.js
@@ -44,5 +44,88 @@ var project_data = {
             {"name":'name',"image":'image'},
         ]
     },
+    'PEM':{
+        "project_name":"Marketing Expense Manager",
+        "logo":"exp_mng.png", //image will be 330*220
+        "url":"https://marketingbudgetsoftware.podareducation.org/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+        ]
+    },
+    
+    'fpg':{
+        "project_name":"FPL London",
+        "logo":"fpg-logo.png", //image will be 330*220
+        "url":"https://fieldsliving.co.uk/",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+        ]
+    },
+    'kf-risk':{
+        "project_name":"Kingfields Risk Management",
+        "logo":"kingfields-logo.png", //image will be 330*220
+        "url":"https://webflix.uk/risk-management/login",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+        ]
+    },
+    'saas-ccp':{
+        "project_name":"CCP",
+        "logo":"jmbaxi-logo.png", //image will be 330*220
+        "url":"https://customercare.jmbaxi.com/login",
+        "private_reason":'0',
+        "organization":"Parasight Solutions",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+        ]
+    },
+
+    'nirvik':{
+        "project_name":"Nirvik grievance filing system",
+        "logo":"nirvik.png", //image will be 330*220
+        "url":"https://customercare.jmbaxi.com/login",
+        "private_reason":'0',
+        "organization":"phoenix infotech",
+        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "description":"chartered studies is an exam portal for ca anspirants",
+        'screenshots':[
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+            {"name":'name',"image":'image'},
+        ]
+    },
+
 
 };
