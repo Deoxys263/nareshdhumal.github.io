41fd0bea3755124d44b875b0ac49b1f8d556e181
deoxys263 <deoxys263@gmail.com>
2025-03-13 23:23:16 +0530

    risk and ccp

--- project_data.js
+++ project_data.js
@@ -92,14 +92,17 @@ var project_data = {
         "url":"https://webflix.uk/risk-management/login",
         "private_reason":'0',
         "organization":"Parasight Solutions",
-        "short_description":"chartered studies is an exam portal for ca anspirants",
-        "description":"chartered studies is an exam portal for ca anspirants",
+        "short_description":"KF Risk Management is a tool for solicitors to analyse risk in cases ",
+        "description":"This application contains different types of forms which helps to find the risk in each case. the forms are msq based as well as different types of input forms and all the questions and its type controlled by admin.",
         'screenshots':[
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
+            {"name":'Login Screen solicitor',"image":'login.png'},
+            {"name":'Solicitor Dashboard',"image":'solic-dash.png'},
+            {"name":'MCQ Based Survey Form',"image":'mcq-form.png'},
+            {"name":'Signature Draw/Upload Area',"image":'Signature-drwapad.png'},
+            {"name":'Dynamic Form',"image":'dynamic.png'},
+            {"name":'Graphocal Report',"image":'graphical-report.png'},
+            {"name":'Questions',"image":'questions.png'},
+            {"name":'Admin Dashboard',"image":'graphical-report.png'},
         ]
     },
     'saas-ccp':{
@@ -108,48 +111,43 @@ var project_data = {
         "url":"https://customercare.jmbaxi.com/login",
         "private_reason":'0',
         "organization":"Parasight Solutions",
-        "short_description":"chartered studies is an exam portal for ca anspirants",
-        "description":"chartered studies is an exam portal for ca anspirants",
+        "short_description":"Customer Complaint Portal is created to solve issues of exporters",
+        "description":"Customer Complaint Portal is laravel based application to solve issues of exporters, recieved suggestions and feedback from customer to all ports of JMBaxi. customer resgister their complaint on portal and port will provide solution to the exportal it follows sequence of approvals to provide end solution.",
         'screenshots':[
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
+            {"name":'Login Screen',"image":'login.png'},
+            {"name":'Customer Dashboard',"image":'cust-dash.png'},
+            {"name":'Create Dispute form',"image":'create-dispute.png'},
+            {"name":'Admin Dashboard',"image":'admin-dash.png'},
+            {"name":'View Dispute',"image":'view-dispute.png'},
+            {"name":'View Dispute - Timeline',"image":'view-dispute-timeline.png'},
+            {"name":'Dispute forward/Action page',"image":'action.png'},
+            {"name":'Chating window for dispute',"image":'chatwindow.png'},
         ]
     },
 
     'nirvik':{
         "project_name":"Nirvik grievance filing system",
         "logo":"nirvik.png", //image will be 330*220
-        "url":"https://customercare.jmbaxi.com/login",
-        "private_reason":'0',
-        "organization":"phoenix infotech",
-        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "url":"",
+        "private_reason":'Appliction link not Available',
+        "organization":"Phoenix infotech",
+        "short_description":"Nirvik GFS is a portal for bank users to solve issues of exporters",
         "description":"chartered studies is an exam portal for ca anspirants",
         'screenshots':[
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
+           
         ]
     },
 
     'ecgc':{
         "project_name":"ECGC Proposal Form",
         "logo":"ecgc.png", //image will be 330*220
-        "url":"https://customercare.jmbaxi.com/login",
-        "private_reason":'0',
+        "url":"",
+        "private_reason":'Appliction link not Available',
         "organization":"phoenix infotech",
-        "short_description":"chartered studies is an exam portal for ca anspirants",
-        "description":"chartered studies is an exam portal for ca anspirants",
+        "short_description":"this application created for digitized  the process of insurance forms",
+        "description":"",
         'screenshots':[
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
+           
         ]
     },
 
@@ -157,16 +155,12 @@ var project_data = {
         "project_name":"NSDL E-Approvals",
         "logo":"nsdl.png", //image will be 330*220
         "url":"https://customercare.jmbaxi.com/login",
-        "private_reason":'0',
+        "private_reason":'Private Application',
         "organization":"NSDL",
-        "short_description":"chartered studies is an exam portal for ca anspirants",
-        "description":"chartered studies is an exam portal for ca anspirants",
+        "short_description":"NSDL E-Approvals digitized the approvals/permssion system ",
+        "description":"",
         'screenshots':[
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
-            {"name":'name',"image":'image'},
+            
         ]
     },
 
@@ -175,10 +169,10 @@ var project_data = {
     'idea-portal':{
         "project_name":"Idea Portal",
         "logo":"jmbaxi-logo.png", //image will be 330*220
-        "url":"https://customercare.jmbaxi.com/login",
-        "private_reason":'0',
+        "url":"",
+        "private_reason":'Application not hosted',
         "organization":"Parasight Solutions",
-        "short_description":"chartered studies is an exam portal for ca anspirants",
+        "short_description":"Idea Portal created for share ideas & impleted them for organization ",
         "description":"chartered studies is an exam portal for ca anspirants",
         'screenshots':[
             {"name":'name',"image":'image'},
